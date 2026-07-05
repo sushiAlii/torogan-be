35e99e602a4c1899c0fdf6892e802d0dc6877735
sushiAlii <aliasgar.laut@gmail.com>
2026-07-06 00:10:12 +0800

    fix: updated users table remove unique clause for role_id remove not null for password add unique for email

--- internal/database/migrations/000004_users.up.sql
+++ internal/database/migrations/000004_users.up.sql
@@ -1,12 +1,12 @@
 CREATE TABLE
     IF NOT EXISTS users (
         id UUID DEFAULT gen_random_uuid() PRIMARY KEY,
-        email VARCHAR(100),
-        password BYTEA NOT NULL,
+        email VARCHAR(100) NOT NULL UNIQUE,
+        password BYTEA,
         avatar_url TEXT,
         created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
         updated_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
         deleted_at TIMESTAMP WITH TIME ZONE,
-        role_id INTEGER REFERENCES roles(id) UNIQUE
+        role_id INTEGER REFERENCES roles(id)
     )
 
