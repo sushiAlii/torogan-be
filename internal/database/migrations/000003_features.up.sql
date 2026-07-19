aceefd836d0eee6ea3923090375598dfb2a2e3d4
sushiAlii <aliasgar.laut@gmail.com>
2026-07-19 16:29:29 +0800

    feat: added predefined features/amenities

--- internal/database/migrations/000003_features.up.sql
+++ internal/database/migrations/000003_features.up.sql
@@ -5,3 +5,19 @@ CREATE TABLE
         created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
         updated_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
     );
+
+INSERT INTO
+    features (name)
+VALUES
+    ('Swimming Pool'),
+    ('Gym'),
+    ('Sauna'),
+    ('Parking'),
+    ('Air Conditioning'),
+    ('WiFi'),
+    ('Balcony'),
+    ('Elevator'),
+    ('24/7 Security'),
+    ('Pet Friendly'),
+    ('Furnished'),
+    ('Backup Power');
